975114c06ca0ed8fb6e2794d9db19cc2c78b4448
simon.ponder <simon.ponder@gmail.com>
2012-02-29 02:19:33 +0000

    Re-integrated branch that implemented changes suggested by Stian. Suggest release.
    * Fixed installed application list, force close.
    * Fixed launch/remove application from installed application list, force close.
    * Main layout now works horizontally.
    * Changed visual interface. 3 page horizontal swipe. Need to inform users of how to interact with this.

--- Fireplace_ad_supported/src/com/fireplace/activity/FireplaceActivity.java
+++ Fireplace_ad_supported/src/com/fireplace/activity/FireplaceActivity.java
@@ -1,24 +1,13 @@
 package com.fireplace.activity;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
@@ -28,10 +17,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -40,10 +27,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -52,9 +36,6 @@ import com.fireplace.software.ChangeLog;
 import com.fireplace.software.ItemSkel;
 import com.google.ads.AdRequest;
 import com.google.ads.AdView;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
 
 public class FireplaceActivity extends ListActivity implements OnClickListener {
 	
@@ -67,8 +48,9 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
 
 	// RECORDING HOW MUCH TIMES BUTTON WAS CLICKED
 	int clickCounter = 1;
-	private EditText etSearchText;
-	AdView adView;
+	AdView adView1;
+	AdView adView2;
+	AdView adView3;
 	final static String TAG = "FireplaceActivity";
 	ArrayList<ItemSkel> list;
 	boolean listReceived = false;
@@ -84,20 +66,24 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
         if (cl.firstRun())
             cl.getLogDialog().show();
 		
-        adView = (AdView) findViewById(R.id.adView);
+        adView1 = (AdView) findViewById(R.id.adView1);
+        adView2 = (AdView) findViewById(R.id.adView2);
+        adView3 = (AdView) findViewById(R.id.adView3);
         
         // Initiate a generic request to load it with an ad
-        adView.loadAd(new AdRequest());
+        adView1.loadAd(new AdRequest());
+        adView2.loadAd(new AdRequest());
+        adView3.loadAd(new AdRequest());
         
 		adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, listItems);
 		setListAdapter(adapter);
-		listItems.add("Network Tools");
-		listItems.add("Root utilities");
-		listItems.add("System Tools");
-		listItems.add("Security");
-		listItems.add("Tweaks");
-		listItems.add("Themes");
+//		listItems.add("Network Tools");
+//		listItems.add("Root utilities");
+//		listItems.add("System Tools");
+//		listItems.add("Security");
+//		listItems.add("Tweaks");
+//		listItems.add("Themes");
 		
 		File folder = new File("/sdcard/Fireplace/");
 
@@ -110,32 +96,32 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
 			}
 		}
 
-		TabHost th = (TabHost) findViewById(R.id.tabhost);
-		th.setup();
-
-		// Tab 1
-		TabSpec ts = th.newTabSpec("tag1"); // ts = TabSpec
-		ts.setContent(R.id.tab1);
-		ts.setIndicator("Home");
-		th.addTab(ts);
-
-		// Tab 2
-		ts = th.newTabSpec("tag2"); // ts = TabSpec
-		ts.setContent(R.id.tab2);
-		ts.setIndicator("Manage");
-		th.addTab(ts);
-
-		// Tab 3
-		ts = th.newTabSpec("tag3"); // ts = TabSpec
-		ts.setContent(R.id.tab3);
-		ts.setIndicator("Browse");
-		th.addTab(ts);
-		
-		// Tab 4
-		ts = th.newTabSpec("tag4"); // ts = TabSpec
-		ts.setContent(R.id.tab4);
-		ts.setIndicator("Search");
-		th.addTab(ts);
+//		TabHost th = (TabHost) findViewById(R.id.tabhost);
+//		th.setup();
+//
+//		// Tab 1
+//		TabSpec ts = th.newTabSpec("tag1"); // ts = TabSpec
+//		ts.setContent(R.id.tab1);
+//		ts.setIndicator("Home");
+//		th.addTab(ts);
+//
+//		// Tab 2
+//		ts = th.newTabSpec("tag2"); // ts = TabSpec
+//		ts.setContent(R.id.tab2);
+//		ts.setIndicator("Manage");
+//		th.addTab(ts);
+//
+//		// Tab 3
+//		ts = th.newTabSpec("tag3"); // ts = TabSpec
+//		ts.setContent(R.id.tab3);
+//		ts.setIndicator("Browse");
+//		th.addTab(ts);
+//		
+//		// Tab 4
+//		ts = th.newTabSpec("tag4"); // ts = TabSpec
+//		ts.setContent(R.id.tab4);
+//		ts.setIndicator("Search");
+//		th.addTab(ts);
 		
 		TextView txtLoading = (TextView) findViewById(R.id.txtLoading);
 		txtLoading.setText(getResources().getString(R.string.strSetUpComp)); // Initial loading
@@ -162,9 +148,9 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
 		txtDeviceInfo.setText("Android: " + android.os.Build.VERSION.RELEASE
 				+ "/ Device: " + android.os.Build.DEVICE);
 		
-		etSearchText = (EditText) findViewById(R.id.etSearchField);
-		Button btnSearch = (Button) findViewById(R.id.btnSearch);
-		btnSearch.setOnClickListener(this);
+//		etSearchText = (EditText) findViewById(R.id.etSearchField);
+//		Button btnSearch = (Button) findViewById(R.id.btnSearch);
+//		btnSearch.setOnClickListener(this);
 
 		txtLoading.setText("Runnning network check");
 
@@ -365,60 +351,14 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
 				
 				break;
 
-			case R.id.btnSearch:
-				
-				
-				
-				break;
+//			case R.id.btnSearch:
+//				
+//				
+//				
+//				break;
 			}
 		}
 	}
-
-	/**
-	 * This Asynchronous task handles retrieving the list items from the server.
-	 *
-	 */
-	private class getListTask extends
-			AsyncTask<String, Integer, ArrayList<ItemSkel>> {
-
-		@Override
-		protected ArrayList<ItemSkel> doInBackground(String... params) {
-			try {
-				HttpClient httpClient = new DefaultHttpClient();
-				HttpContext localContext = new BasicHttpContext();
-				HttpGet httpGet = new HttpGet(
-						"http://www.fireplace-market.com/getdata.php");
-				HttpResponse response = httpClient.execute(httpGet,
-						localContext);
-				String result = "";
-
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(response.getEntity().getContent()));
-
-				String line = null;
-				while ((line = reader.readLine()) != null)
-					result += line;
-
-				Type type = new TypeToken<ArrayList<ItemSkel>>() {
-				}.getType();
-				Gson g = new Gson();
-				list = g.fromJson(result, type);
-				listReceived = true;				
-				return list;
-			} catch (JsonSyntaxException e) {
-				Log.e(TAG, e.getMessage());
-			} catch (ClientProtocolException e) {
-				Log.e(TAG, e.getMessage());
-			} catch (IllegalStateException e) {
-				Log.e(TAG, e.getMessage());
-			} catch (IOException e) {
-				Log.e(TAG, e.getMessage());
-			}
-			return null;
-		}
-
-	}
-
 	
 	public boolean isOnline() {
 		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -446,108 +386,110 @@ public class FireplaceActivity extends ListActivity implements OnClickListener {
 		return false;
 	}
 
-	public boolean updateChecketwork() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		if (isOnline()) {
-			try {
-
-				// set the download URL, a url that points to a file on the
-				// internet
-				// this is the file to be downloaded
-				// Toast.makeText(this, "Preparing: Packages",
-				// Toast.LENGTH_SHORT).show();
-				String updateString = getString(R.string.updateTo);
-				URL url = new URL(
-						"http://www.u2worlds.com/fp/updates/Fireplace_update"
-								+ updateString + ".apk");
-
-				// create the new connection
-				HttpURLConnection urlConnection = (HttpURLConnection) url
-						.openConnection();
-
-				// set up some things on the connection
-				urlConnection.setRequestMethod("GET");
-				urlConnection.setDoOutput(true);
-
-				// and connect!
-				urlConnection.connect();
-
-				// set the path where we want to save the file
-				// in this case, going to save it on the root directory of the
-				// sd card.
-
-				File SDCardRoot = Environment.getExternalStorageDirectory();
-				// create a new file, specifying the path, and the filename
-				// which we want to save the file as.
-				File file = new File(SDCardRoot + "/Fireplace/Fireplace_update"
-						+ updateString + ".apk");
-
-				// this will be used to write the downloaded data into the file
-				// we created
-				FileOutputStream fileOutput = new FileOutputStream(file);
-
-				// this will be used in reading the data from the internet
-				InputStream inputStream = urlConnection.getInputStream();
-
-				// this is the total size of the file
-				int totalSize = urlConnection.getContentLength();
-				// variable to store total downloaded bytes
-				int downloadedSize = 0;
-
-				// create a buffer...
-				byte[] buffer = new byte[1024];
-				int bufferLength = 0; // used to store a temporary size of the
-										// buffer
-
-				// now, read through the input buffer and write the contents to
-				// the file
-				while ((bufferLength = inputStream.read(buffer)) > 0) {
-					// add the data in the buffer to the file in the file output
-					// stream (the file on the sd card
-					fileOutput.write(buffer, 0, bufferLength);
-					// add up the size so we know how much is downloaded
-					downloadedSize += bufferLength;
-					// this is where you would do something to report the
-					// prgress, like this maybe
-					updateProgress(downloadedSize, totalSize);
-
-				}
-				// close the output stream when done
-				fileOutput.close();
-
-				// catch some possible errors...
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			String updateString = getString(R.string.updateTo);
-
-			File appFile = new File("/sdcard/Fireplace/Fireplace_update"
-					+ updateString + ".apk");
-			Intent installIntent = new Intent(Intent.ACTION_VIEW);
-			installIntent.setDataAndType(Uri.fromFile(appFile),
-					"application/vnd.android.package-archive");
-			startActivity(installIntent);
-
-			return true;
-		}
-		Toast.makeText(FireplaceActivity.this, "No update available",
-				Toast.LENGTH_LONG).show();
-		return false;
-	}
+//	public boolean updateCheckNetwork() {
+//		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+//		if (isOnline()) {
+//			try {
+//
+//				// set the download URL, a url that points to a file on the
+//				// internet
+//				// this is the file to be downloaded
+//				// Toast.makeText(this, "Preparing: Packages",
+//				// Toast.LENGTH_SHORT).show();
+//				String updateString = getString(R.string.updateTo);
+//				URL url = new URL(
+//						"http://www.u2worlds.com/fp/updates/Fireplace_update"
+//								+ updateString + ".apk");
+//
+//				// create the new connection
+//				HttpURLConnection urlConnection = (HttpURLConnection) url
+//						.openConnection();
+//
+//				// set up some things on the connection
+//				urlConnection.setRequestMethod("GET");
+//				urlConnection.setDoOutput(true);
+//
+//				// and connect!
+//				urlConnection.connect();
+//
+//				// set the path where we want to save the file
+//				// in this case, going to save it on the root directory of the
+//				// sd card.
+//
+//				File SDCardRoot = Environment.getExternalStorageDirectory();
+//				// create a new file, specifying the path, and the filename
+//				// which we want to save the file as.
+//				File file = new File(SDCardRoot + "/Fireplace/Fireplace_update"
+//						+ updateString + ".apk");
+//
+//				// this will be used to write the downloaded data into the file
+//				// we created
+//				FileOutputStream fileOutput = new FileOutputStream(file);
+//
+//				// this will be used in reading the data from the internet
+//				InputStream inputStream = urlConnection.getInputStream();
+//
+//				// this is the total size of the file
+//				int totalSize = urlConnection.getContentLength();
+//				// variable to store total downloaded bytes
+//				int downloadedSize = 0;
+//
+//				// create a buffer...
+//				byte[] buffer = new byte[1024];
+//				int bufferLength = 0; // used to store a temporary size of the
+//										// buffer
+//
+//				// now, read through the input buffer and write the contents to
+//				// the file
+//				while ((bufferLength = inputStream.read(buffer)) > 0) {
+//					// add the data in the buffer to the file in the file output
+//					// stream (the file on the sd card
+//					fileOutput.write(buffer, 0, bufferLength);
+//					// add up the size so we know how much is downloaded
+//					downloadedSize += bufferLength;
+//					// this is where you would do something to report the
+//					// prgress, like this maybe
+//					updateProgress(downloadedSize, totalSize);
+//
+//				}
+//				// close the output stream when done
+//				fileOutput.close();
+//
+//				// catch some possible errors...
+//			} catch (MalformedURLException e) {
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//
+//			String updateString = getString(R.string.updateTo);
+//
+//			File appFile = new File("/sdcard/Fireplace/Fireplace_update"
+//					+ updateString + ".apk");
+//			Intent installIntent = new Intent(Intent.ACTION_VIEW);
+//			installIntent.setDataAndType(Uri.fromFile(appFile),
+//					"application/vnd.android.package-archive");
+//			startActivity(installIntent);
+//
+//			return true;
+//		}
+//		Toast.makeText(FireplaceActivity.this, "No update available",
+//				Toast.LENGTH_LONG).show();
+//		return false;
+//	}
 	
 	@Override
 	protected void onStart() {
 		super.onStart();
-		etSearchText.setText("");
+//		etSearchText.setText("");
 	}
 	
 	@Override
 	  public void onDestroy() {
-	    adView.destroy();
+	    adView1.destroy();
+	    adView2.destroy();
+	    adView3.destroy();
 	    super.onDestroy();
 	  }
 
